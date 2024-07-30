1ef3a7480818b658bfbc4aae0770dc33f033896d
KDaria <dkdva@mail.ru>
2024-07-30 17:50:57 +0200

    Initial Commit

---
+++ MVVMApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMApp
+//
+//  Created by Дарья Кобелева on 30.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
